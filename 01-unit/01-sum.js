89440526078d926d08b0de681353614b26d9c4ef
christafayereyes <christafayereyes@gmail.com>
2021-11-16 12:32:55 -0600

    Added Test Cases 3 and 4

--- 01-unit/01-sum.js
+++ 01-unit/01-sum.js
@@ -1,7 +1,7 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function sum(num1, num2) {
+function sum(num1=0, num2=0) {
   return result = num1 + num2
 }
 
@@ -27,11 +27,15 @@ try {
   // Test Case 3
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
-
+  var result = sum(1);
+  if (result !== 1) throw new Error('Expected sum(1) to be 1. Received: ' + result);
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
+  var result = sum();
+  if (result !== 0) throw new Error('Expected sum() to be 0. Received: ' + result);
+
 
   console.log('All tests passed successfully.');
 
